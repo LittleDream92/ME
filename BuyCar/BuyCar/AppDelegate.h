25b860d698f3c87a3a214028c1e013114419f185
Your Name <LittleDream92>
2017-03-30 11:56:07 +0800

    项目

---
+++ BuyCar/BuyCar/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BuyCar
+//
+//  Created by Meng Fan on 17/3/30.
+//  Copyright © 2017年 Meng Fan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BuyCar/BuyCar/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BuyCar
+//
+//  Created by Meng Fan on 17/3/30.
+//  Copyright © 2017年 Meng Fan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
